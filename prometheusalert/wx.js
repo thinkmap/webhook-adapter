1fde89695157d85f8f75b7945c878626235d03ae
thinkmap <37236741+thinkmap@users.noreply.github.com>
2022-11-10 15:44:56 +0800

    Update wx.js

--- prometheusalert/wx.js
+++ prometheusalert/wx.js
@@ -12,16 +12,16 @@ const formatTimeStamp = (timeStamp) => {
 
 exports.template = function(body) {
     //企业微信群机器人API，https://work.weixin.qq.com/help?person_id=1&doc_id=13376#markdown%E7%B1%BB%E5%9E%8B
-    //prometheus alert manager webhook ： https://prometheus.io/docs/alerting/configuration/#webhook_config
+    //prometheus alert manager webhook: https://prometheus.io/docs/alerting/configuration/#webhook_config
     var alerts = body.alerts;
+    if ((body.status)=="firing") {
     var content = alerts.map(
         alert => {
-            return [`# Name:${alert.labels.alertname}`, "## Labels:"]
+            return [`## Name:${alert.labels.alertname}`, "### Labels:"]
             .concat(Object.entries(alert.labels).map(label => `<font color="comment">${label[0]}:</font>${label[1]}`))
-            .concat("## Annotations:")
+            .concat("### Annotations:")
             .concat(Object.entries(alert.annotations).map(annotation => `<font color="comment">${annotation[0]}:</font>${annotation[1]}`))
             .concat(`###### 故障时间:<font color="warning">${formatTimeStamp((Date.parse(alert.startsAt)))}</font>`)
-            .concat(`###### 恢复时间:<font color="info">${formatTimeStamp((Date.parse(alert.endsAt)))}</font>`)
             .join("\n")
         }
     ).concat(`<font color="comment">告警状态:</font><font color="${body.status === 'firing' ? 'warning' : 'info'}">${body.status}</font>`).join("\n\n");
@@ -31,5 +31,26 @@ exports.template = function(body) {
         markdown: {
             content: content
         }
+    }
+    }
+    if ((body.status)=="resolved") {
+    var content = alerts.map(
+        alert => {
+            return [`## Name: ${alert.labels.alertname}`, "### Labels:"]
+            .concat(Object.entries(alert.labels).map(label => `<font color="comment">${label[0]}: </font>${label[1]}`))
+            .concat("### Annotations:")
+            .concat(Object.entries(alert.annotations).map(annotation => `<font color="comment">${annotation[0]}: </font>${annotation[1]}`))
+            .concat(`###### 故障时间: <font color="warning">${formatTimeStamp((Date.parse(alert.startsAt)))}</font>`)
+            .concat(`###### 恢复时间: <font color="info">${formatTimeStamp((Date.parse(alert.endsAt)))}</font>`)
+            .join("\n")
+        }
+    ).concat(`<font color="comment">告警状态: </font><font color="${body.status === 'firing' ? 'warning' : 'info'}">${body.status}</font>`).join("\n\n");
+    return {
+        
+        msgtype: "markdown",
+        markdown: {
+            content: content
+        }
+    }
     }
 }
